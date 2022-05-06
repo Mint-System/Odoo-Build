7ed463073ea3bc291164aa1fed40ca0b22198050
janikvonrotz <contact@janikvonrotz.ch>
2022-05-06 17:03:36 +0200

    add url and logo

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
         ['link', { rel: "icon", type: "image/png", href: "icon.png"}],
     ],
     themeConfig: {
+        logo: '/icon.png',
         sidebar: 'auto',
         repo: 'mint-system/odoo-development',
         docsBranch: '14.0',
@@ -12,7 +13,8 @@ module.exports = {
         nav: [
             { text: 'Home', link: '/' },
             { text: 'Help', link: '/task' },
-            { text: 'Snippets', link: '/snippets' }
+            { text: 'Snippets', link: '/snippets' },
+            { text: 'Mint System', link: 'https://www.mint-system.ch' }
         ],
     },
     dest: 'public',
