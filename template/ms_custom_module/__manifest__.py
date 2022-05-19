bb9e267f30a7076195cc115c1709b2f56189c984
janikvonrotz <contact@janikvonrotz.ch>
2022-05-19 19:39:59 +0200

    sync from 14.0

--- template/ms_custom_module/__manifest__.py
+++ template/ms_custom_module/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Purchase,Technical,Accounting,Invoicing,Sales,Human Resources,Manufacturing,Website,Inventory,Administration,Tools",
-    "version": "14.0.0.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["base"],
     "data": [
