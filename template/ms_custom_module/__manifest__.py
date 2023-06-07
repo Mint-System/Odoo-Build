1b9298e8951145b57a8350ae126872cc9d4d494c
Janik von Rotz <login@janikvonrotz.ch>
2023-06-07 18:44:57 +0200

    update module template

--- template/ms_custom_module/__manifest__.py
+++ template/ms_custom_module/__manifest__.py
@@ -5,7 +5,7 @@
     """,
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
-    "category": "Purchase,Technical,Accounting,Invoicing,Sales,Human Resources,Manufacturing,Website,Inventory,Administration,Tools",
+    "category": "Purchase,Technical,Accounting,Invoicing,Sales,Human Resources,Services,Helpdesk,Manufacturing,Website,Inventory,Administration,Productivity",
     "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["base"],
