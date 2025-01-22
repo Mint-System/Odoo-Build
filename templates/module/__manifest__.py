b3f81b6228799441b3a13ccd0ccdbe1463e2e780
Janik von Rotz <login@janikvonrotz.ch>
2025-01-22 10:21:23 +0100

    feat: ref

--- templates/module/__manifest__.py
+++ templates/module/__manifest__.py
@@ -5,7 +5,7 @@
     """,
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
-    "category": "Purchase,Technical,Accounting,Invoicing,Sales,Human Resources,Services,Helpdesk,Manufacturing,Website,Inventory,Administration,Productivity",
+    "category": "Repository",
     "version": "16.0.1.0.0",
     "license": "AGPL-3,OPL-1",
     "depends": ["base"],
