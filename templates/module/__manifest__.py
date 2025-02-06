e06cff1152b53cf3f45d0af3cc9791e1b1a87099
Janik von Rotz <login@janikvonrotz.ch>
2025-02-06 13:44:13 +0100

    fix: author in template files

--- templates/module/__manifest__.py
+++ templates/module/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Module summary.
     """,
-    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "author": "Mint System GmbH",
     "website": "https://www.mint-system.ch",
     "category": "Repository",
     "version": "16.0.1.0.0",
