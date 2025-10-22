6fc05e6277b34bb5552f22d038b14be3c0649670
Janik von Rotz <login@janikvonrotz.ch>
2025-10-22 17:20:24 +0200

    fix(templates): add pre-migrate.py to rename model and field in sale_subscription module

---
+++ templates/migrations/pre-migrate.py
@@ -0,0 +1,17 @@
+from odoo.addons.upgrade_util import util
+
+
+def migrate(cr, version):
+    util.rename_model(cr, "sale.temporal.period_discount", "sale.subscription.period_discount")
+
+    util.rename_field(cr, "sale.subscription.period_discount", "recurrence_id", "plan_id")
+
+    util.rename_xmlid(
+        cr,
+        [
+            (
+                "sale_subscription.sale_temporal_recurrence_view_form",
+                "sale_subscription.sale_subscription_plan_view_form",
+            ),
+        ],
+    )
