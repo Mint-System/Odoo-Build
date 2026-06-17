e01d9463d560175d519a4ca69cbfe0412af37a09
Janik von Rotz <login@janikvonrotz.ch>
2026-06-17 12:17:48 +0200

    feat(website_blog): implement blog feed endpoint with Atom XML rendering

--- templates/main.py
+++ templates/main.py
@@ -1,20 +1,20 @@
 from werkzeug.exceptions import NotFound
 
 from odoo import http
+from odoo.http import request
+from odoo.tools import html2plaintext
 
 from odoo.addons.website_blog.controllers.main import WebsiteBlog
 
 
-class WebsiteBlog(WebsiteBlog):
-    @http.route([
-        '/blog',
-        '/blog/page/<int:page>',
-        '/blog/tag/<string:tag>',
-        '/blog/tag/<string:tag>/page/<int:page>',
-        '''/blog/<model("blog.blog"):blog>''',
-        '''/blog/<model("blog.blog"):blog>/page/<int:page>''',
-        '''/blog/<model("blog.blog"):blog>/tag/<string:tag>''',
-        '''/blog/<model("blog.blog"):blog>/tag/<string:tag>/page/<int:page>''',
-    ], type='http', auth="public", website=True, sitemap=True)
-    def blog(self, blog=None, tag=None, page=1, search=None, **opt):
-        return
+class WebsiteBlog(http.Controller):
+
+    @http.route(['''/blog/<model("blog.blog"):blog>/feed'''], type='http', auth="public", website=True, sitemap=True)
+    def blog_feed(self, blog, limit='15', **kwargs):
+        v = {}
+        v['blog'] = blog
+        v['base_url'] = blog.get_base_url()
+        v['posts'] = request.env['blog.post'].search([('blog_id', '=', blog.id)], limit=min(int(limit), 50), order="post_date DESC")
+        v['html2plaintext'] = html2plaintext
+        r = request.render("website_blog.blog_feed", v, headers=[('Content-Type', 'application/atom+xml')])
+        return r
